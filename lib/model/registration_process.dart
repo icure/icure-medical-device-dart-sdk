68f7de5f1ae9710bf42752cbf73fe0ecd6fdb125
Antoine Duchâteau <ad@taktik.com>
2022-02-23 12:59:41 +0100

    Added simplified calls for registration

---
+++ lib/model/registration_process.dart
@@ -0,0 +1,11 @@
+//
+// @dart=2.12
+
+part of icure_medical_device_dart_sdk.api;
+
+class RegistrationProcess {
+  RegistrationProcess(this.processId, this.login);
+
+  String processId;
+  String login;
+}
