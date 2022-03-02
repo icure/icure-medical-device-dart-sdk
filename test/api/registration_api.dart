68316fa845f055db533cc5ba244736a69b8a5ee0
Clément Vandendaelen <cv@taktik.com>
2022-03-02 14:03:10 +0100

    Removed unused stuff in registration_api.dart test

--- test/api/registration_api.dart
+++ test/api/registration_api.dart
@@ -3,16 +3,11 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
-import 'package:icure_dart_sdk/api.dart' as rapi;
 import 'package:icure_dart_sdk/util/binary_utils.dart';
 import 'package:icure_medical_device_dart_sdk/api.dart';
 import "package:test/test.dart";
-import 'package:uuid/uuid.dart';
-import 'package:uuid/uuid_util.dart';
 
 void main() {
-  final Uuid uuid = Uuid();
-
   Future<MedTechApi> medtechApi() async {
     var fileUri = Uri.file("test/resources/keys/782f1bcd-9f3f-408a-af1b-cd9f3f908a98-icc-priv.2048.key", windows: false);
     var hcpKeyFile = File.fromUri(fileUri);
@@ -26,9 +21,6 @@ void main() {
         .addKeyPair("782f1bcd-9f3f-408a-af1b-cd9f3f908a98", (await hcpKeyFile.readAsString(encoding: utf8)).keyFromHexString())
         .build();
   }
-
-  rapi.DecryptedPatientDto getPatient() => rapi.DecryptedPatientDto(
-      id: uuid.v4(options: {'rng': UuidUtil.cryptoRNG}), firstName: 'John', lastName: 'Doe', note: 'Premature optimization is the root of all evil');
 
   group('tests for RegistrationApi', () {
     test('test signup', () async {
