51e81aab4af5abb650372a63db04181f589620e3
Lisa Trapletti <lt@icure.io>
2022-03-02 17:38:52 +0100

    Add test to create data samples for patient when logged as user patient

--- test/api/data_sample_api.dart
+++ test/api/data_sample_api.dart
@@ -13,39 +13,52 @@ void main() {
   final Uuid uuid = Uuid();
 
   Future<MedTechApi> medtechApi() async {
-    final MedTechApiBuilder builder = MedTechApiBuilder()
-        .withICureBasePath("https://kraken.icure.dev")
-        .withUserName("abdemotst2")
-        .withPassword("27b90f6e-6847-44bf-b90f-6e6847b4bf1c")
-        .withMsgGtwUrl("https://msg-gw.icure.cloud/km")
-        .withSignUpProcessId("f0ced6c6-d7cb-4f78-841e-2674ad09621e");
-
-    var fileUri = Uri.file("test/resources/keys/782f1bcd-9f3f-408a-af1b-cd9f3f908a98-icc-priv.2048.key", windows: false);
+    var fileUri = Uri.file("test/resources/keys/a37e0a71-07d2-4414-9b2b-2120ae9a16fc-icc-priv.2048.key", windows: false);
     var hcpKeyFile = File.fromUri(fileUri);
 
-    builder.addKeyPair("782f1bcd-9f3f-408a-af1b-cd9f3f908a98", (await hcpKeyFile.readAsString(encoding: utf8)).keyFromHexString());
+    final MedTechApiBuilder builder = MedTechApiBuilder()
+        .withICureBasePath("https://kraken.icure.dev")
+        .withUserName("XXX")
+        .withPassword("XXX")
+        .withMsgGtwUrl("https://msg-gw.icure.cloud/km")
+        .withSignUpProcessId("f0ced6c6-d7cb-4f78-841e-2674ad09621e")
+        .addKeyPair("a37e0a71-07d2-4414-9b2b-2120ae9a16fc", (await hcpKeyFile.readAsString(encoding: utf8)).keyFromHexString());
 
     return builder.build();
   }
 
   DataSample getHeightDataSample() => DataSample(
-        id: uuid.v4(options: {'rng': UuidUtil.cryptoRNG}),
-        content: {"en": Content(numberValue: 159.0)},
-        valueDate: 20220203111128,
-        labels: [CodingReference(id: "LOINC|8302-2|2", code: "8302-2", type: "LOINC", version: "2")].toSet(),
-      );
+    id: uuid.v4(options: {'rng': UuidUtil.cryptoRNG}),
+    content: {"en": Content(numberValue: 159.0)},
+    valueDate: 20220203111128,
+    labels: [CodingReference(id: "LOINC|8302-2|2", code: "8302-2", type: "LOINC", version: "2")].toSet(),
+  );
 
   DataSample getWeightDataSample() => DataSample(
-        id: uuid.v4(options: {'rng': UuidUtil.cryptoRNG}),
-        content: {"en": Content(numberValue: 53.5)},
-        valueDate: 20220203111034,
-        labels: [CodingReference(id: "LOINC|29463-7|2", code: "29463-7", type: "LOINC", version: "2")].toSet(),
-      );
+    id: uuid.v4(options: {'rng': UuidUtil.cryptoRNG}),
+    content: {"en": Content(numberValue: 53.5)},
+    valueDate: 20220203111034,
+    labels: [CodingReference(id: "LOINC|29463-7|2", code: "29463-7", type: "LOINC", version: "2")].toSet(),
+  );
 
   Patient getPatient() => Patient(id: uuid.v4(options: {'rng': UuidUtil.cryptoRNG}), firstName: "Raymond", lastName: "Jamonserano");
   Patient getEmptyPatient() => Patient(id: uuid.v4(options: {'rng': UuidUtil.cryptoRNG}), firstName: "Raymond", lastName: "Jambomaigre");
 
   group('tests for DataSampleApi', () {
+    test('test add data samples to HK patient', () async {
+      // Init
+      final MedTechApi patApi = await medtechApi();
+      final DataSampleApi patDataSampleApi = DataSampleApiImpl(patApi);
+
+      final DataSample weight = getWeightDataSample();
+      final DataSample height = getHeightDataSample();
+      final dataSamples = [weight, height];
+
+      final patDataSamples = await patDataSampleApi.createOrModifyDataSamplesFor("a37e0a71-07d2-4414-9b2b-2120ae9a16fc", dataSamples);
+
+      assert(patDataSamples != null);
+    });
+
     test('test createOrModifyDataSampleFor CREATE', () async {
       // Init
       final MedTechApi api = await medtechApi();
@@ -54,10 +67,9 @@ void main() {
       final DataSample weight = getWeightDataSample();
       final DataSample height = getHeightDataSample();
       final dataSamples = [weight, height];
-      final Patient patient = getPatient();
 
       // When
-      final createdPatient = await patientApi.createOrModifyPatient(patient);
+      final createdPatient = await patientApi.getPatient("a37e0a71-07d2-4414-9b2b-2120ae9a16fc");
       final createdDataSamples = await dataSampleApi.createOrModifyDataSamplesFor(createdPatient!.id!, dataSamples);
       print(createdDataSamples?.map((e) => e.id));
 
@@ -121,15 +133,15 @@ void main() {
       final createdDataSample = (await dataSampleApi.createOrModifyDataSampleFor(createdPatient!.id!, weight))!;
       var paginatedListDataSample = await dataSampleApi.filterDataSample(
           await DataSampleFilter()
-          .forDataOwner((await api.userApi.getLoggedUser())!.healthcarePartyId!)
-          .forPatients(api.localCrypto, [createdPatient]).build()
+              .forDataOwner((await api.userApi.getLoggedUser())!.healthcarePartyId!)
+              .forPatients(api.localCrypto, [createdPatient]).build()
       );
       final gotDataSample = (paginatedListDataSample)!.rows[0];
 
       // Then
-      assert(gotDataSample!.id != null && gotDataSample.id == createdDataSample.id);
-      assert(createdDataSample.batchId == gotDataSample!.batchId);
-      assert(createdDataSample.author == gotDataSample!.author);
+      assert(gotDataSample.id != null && gotDataSample.id == createdDataSample.id);
+      assert(createdDataSample.batchId == gotDataSample.batchId);
+      assert(createdDataSample.author == gotDataSample.author);
     });
 
     test('test deleteDataSample', () async {
